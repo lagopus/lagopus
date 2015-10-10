ebe49c0d0e15011b3f02ced84930caa3b4f8bfe0
hibitomo <hibi.tomoya@lab.ntt.co.jp>
2015-10-11 03:34:30 +0900

    dataplane: Add benchmark test with multiple packet flow.

--- src/dataplane/ofproto/test/benchmark_test.c
+++ src/dataplane/ofproto/test/benchmark_test.c
@@ -22,6 +22,7 @@
 #include "datapath_test_misc.h"
 
 static struct bridge *bridge;
+static struct flowcache *flowcache;
 bool loop;
 
 void
@@ -41,6 +42,8 @@ setUp(void) {
   TEST_ASSERT_EQUAL(dp_bridge_port_set("br0", "port1", 2), LAGOPUS_RESULT_OK);
   bridge = dp_bridge_lookup("br0");
   TEST_ASSERT_NOT_NULL(bridge);
+  flowcache = init_flowcache(FLOWCACHE_HASHMAP_NOLOCK);
+  TEST_ASSERT_NOT_NULL(flowcache);
 }
 
 void
@@ -51,8 +54,17 @@ tearDown(void) {
   TEST_ASSERT_EQUAL(dp_port_destroy("port1"), LAGOPUS_RESULT_OK);
   TEST_ASSERT_EQUAL(dp_bridge_destroy("br0"), LAGOPUS_RESULT_OK);
   bridge = NULL;
+  fini_flowcache(flowcache);
 
   dp_api_fini();
+}
+
+void
+print_flowcache_stats(void) {
+
+  printf("flowcache stats: entry:%u, hit:%u, miss:%u, ratio %f%%\n",
+         flowcache->nentries, flowcache->hit, flowcache->miss,
+         (double)flowcache->hit / (double)(flowcache->hit + flowcache->miss));
 }
 
 void
@@ -191,11 +203,12 @@ mpls_flow_add(uint32_t label_start, uint32_t label_end) {
 void
 ipsrc_flow_add(uint32_t ip_start, uint32_t ip_end, int prefix) {
   struct match_list match_list;
-  uint32_t ip, mask;
+  uint32_t ip, mask, count;
 
-  mask = (1 << (32 - prefix)) - 1;
+  count = (1 << (32 - prefix));
+  mask = ~(count - 1);
   if (ip_start <= ip_end) {
-    for (ip = ip_start; ip <= ip_end; ip++) {
+    for (ip = ip_start; ip <= ip_end; ip += count) {
       TAILQ_INIT(&match_list);
       if (prefix == 32) {
         any_flow_add(2,
@@ -226,11 +239,12 @@ ipsrc_flow_add(uint32_t ip_start, uint32_t ip_end, int prefix) {
 void
 ipdst_flow_add(uint32_t ip_start, uint32_t ip_end, int prefix) {
   struct match_list match_list;
-  uint32_t ip, mask;
+  uint32_t ip, mask, count;
 
-  mask = (1 << (32 - prefix)) - 1;
+  count = (1 << (32 - prefix));
+  mask = ~(count - 1);
   if (ip_start <= ip_end) {
-    for (ip = ip_start; ip <= ip_end; ip++) {
+    for (ip = ip_start; ip <= ip_end; ip += count) {
       TAILQ_INIT(&match_list);
       if (prefix == 32) {
         any_flow_add(2,
@@ -361,22 +375,20 @@ build_ip_packet(uint32_t in_port, uint32_t ipsrc, uint32_t ipdst,
 
 void
 cache_enable(struct lagopus_packet *pkt) {
-  pkt->cache = init_flowcache(FLOWCACHE_HASHMAP_NOLOCK);
+  pkt->cache = flowcache;
 }
 
 void
 destroy_packet(struct lagopus_packet *pkt) {
-  if (pkt->cache != NULL) {
-    fini_flowcache(pkt->cache);
-  }
   lagopus_packet_free(pkt);
 }
 
 void
-flow_benchmark(struct lagopus_packet *pkt, time_t sec) {
+flow_benchmark(struct lagopus_packet *pkts[], size_t npkts, time_t sec) {
   struct flowdb *flowdb;
   struct table *table;
   struct flow *flow;
+  struct lagopus_packet *pkt;
   const struct cache_entry *cache_entry;
   uint64_t lookup_count, match_count;
 
@@ -385,8 +397,8 @@ flow_benchmark(struct lagopus_packet *pkt, time_t sec) {
   match_count = 0;
 
 #ifdef USE_MBTREE
-  flowdb = pkt->in_port->bridge->flowdb;
-  table = table_lookup(flowdb, pkt->table_id);
+  flowdb = pkts[0]->in_port->bridge->flowdb;
+  table = table_lookup(flowdb, pkts[0]->table_id);
   if (table->flow_list->type == 0) {
     cleanup_mbtree(table->flow_list);
     build_mbtree(table->flow_list);
@@ -396,19 +408,24 @@ flow_benchmark(struct lagopus_packet *pkt, time_t sec) {
   set_timer(sec);
   /* lookup loop */
   while (loop == true) {
-    flowdb = pkt->in_port->bridge->flowdb;
-    table = table_lookup(flowdb, pkt->table_id);
-    cache_entry = cache_lookup(pkt->cache, pkt);
-    if (likely(cache_entry != NULL)) {
-      lookup_count++;
-      match_count++;
-    } else {
-      flow = lagopus_find_flow(pkt, table);
-      lookup_count++;
-      if (flow != NULL) {
+    size_t i;
+
+    for (i = 0; i < npkts && loop == true; i++) {
+      pkt = pkts[i];
+      flowdb = pkt->in_port->bridge->flowdb;
+      table = table_lookup(flowdb, pkt->table_id);
+      cache_entry = cache_lookup(pkt->cache, pkt);
+      if (likely(cache_entry != NULL)) {
+        lookup_count++;
         match_count++;
-        if (pkt->cache != NULL) {
-          register_cache(pkt->cache, pkt->hash64, 1, &flow);
+      } else {
+        flow = lagopus_find_flow(pkt, table);
+        lookup_count++;
+        if (flow != NULL) {
+          match_count++;
+          if (pkt->cache != NULL) {
+            register_cache(pkt->cache, pkt->hash64, 1, &flow);
+          }
         }
       }
     }
@@ -426,10 +443,11 @@ test_1_entry_flow_benchmark(void) {
   printf("\n*** 1 entry (in_port = 1)\n");
   in_port_flow_add(1, 1);
   pkt = build_packet(1);
-  flow_benchmark(pkt, 1);
+  flow_benchmark(&pkt, 1, 1);
   printf("\n*** cached 1 entry (in_port = 1)\n");
   cache_enable(pkt);
-  flow_benchmark(pkt, 1);
+  flow_benchmark(&pkt, 1, 1);
+  print_flowcache_stats();
   destroy_packet(pkt);
 }
 
@@ -440,10 +458,11 @@ test_2_1_entries_flow_benchmark(void) {
   printf("\n*** 2 entries (in_port = 2 -> 1)\n");
   in_port_flow_add(256, 1);
   pkt = build_packet(1);
-  flow_benchmark(pkt, 1);
+  flow_benchmark(&pkt, 1, 1);
   printf("\n*** cached 2 entries (in_port = 2 -> 1)\n");
   cache_enable(pkt);
-  flow_benchmark(pkt, 1);
+  flow_benchmark(&pkt, 1, 1);
+  print_flowcache_stats();
   destroy_packet(pkt);
 }
 
@@ -454,52 +473,116 @@ test_256_1_entries_flow_benchmark(void) {
   printf("\n*** 256 entries (in_port = 256 -> 1)\n");
   in_port_flow_add(256, 1);
   pkt = build_packet(1);
-  flow_benchmark(pkt, 1);
+  flow_benchmark(&pkt, 1, 1);
   printf("\n*** cached 256 entries (in_port = 256 -> 1)\n");
   cache_enable(pkt);
-  flow_benchmark(pkt, 1);
+  flow_benchmark(&pkt, 1, 1);
+  print_flowcache_stats();
   destroy_packet(pkt);
+}
+
+void
+MPLS_1M_entries_n_flow_benchmark(size_t n) {
+  struct lagopus_packet *pkt[n];
+  size_t i;
+
+  printf("\n*** 1M entries %u flow(s) (MPLS label = 1M -> 1)\n", n);
+  mpls_flow_add(1000 * 1000, 1);
+  for (i = 0; i < n; i++) {
+    pkt[i] = build_mpls_packet(1, i + 1);
+  }
+  flow_benchmark(pkt, n, 1);
+  printf("\n*** cached 1M entries %u flow(s) (MPLS label = 1M -> 1)\n", n);
+  for (i = 0; i < n; i++) {
+    cache_enable(pkt[i]);
+  }
+  flow_benchmark(pkt, n, 1);
+  print_flowcache_stats();
+  for (i = 0; i < n; i++) {
+    destroy_packet(pkt[i]);
+  }
 }
 
 void
 test_MPLS_1M_entries_flow_benchmark(void) {
-  struct lagopus_packet *pkt;
+  MPLS_1M_entries_n_flow_benchmark(1);
+}
 
-  printf("\n*** 1M entries (MPLS label = 1M -> 1)\n");
-  mpls_flow_add(1000 * 1000, 1);
-  pkt = build_mpls_packet(1, 100);
-  flow_benchmark(pkt, 1);
-  printf("\n*** cached 1M entries (MPLS label = 1M -> 1)\n");
-  cache_enable(pkt);
-  flow_benchmark(pkt, 1);
-  destroy_packet(pkt);
+void
+test_MPLS_1M_entries_1000_flow_benchmark(void) {
+  MPLS_1M_entries_n_flow_benchmark(1000);
+}
+
+void
+test_MPLS_1M_entries_1M_flow_benchmark(void) {
+  MPLS_1M_entries_n_flow_benchmark(1000 * 1000);
 }
 
 #define IPADDR(a,b,c,d) (((a)<<24)|((b)<<16)|((c)<<8)|(d))
 void
-test_IPV4_SRC_65536_entries_flow_benchmark(void) {
-  struct lagopus_packet *pkt;
+IPV4_SRC_65536_entries_flow_benchmark(size_t n) {
+  struct lagopus_packet *pkt[n];
+  size_t i;
 
-  printf("\n*** 65536 entries (IPV4_SRC 192.168.0.0-192.168.255.255)\n");
+  printf("\n*** 65536 entries %u flow(s) "
+         "(IPV4_SRC 192.168.0.0-192.168.255.255)\n", n);
   ipsrc_flow_add(IPADDR(192,168,0,0), IPADDR(192,168,255,255), 32);
-  pkt = build_ip_packet(1, IPADDR(192,168,1,1), IPADDR(192,168,1,2), 0);
-  flow_benchmark(pkt, 1);
-  printf("\n*** cached 65536 entries (IPV4_SRC 192.168.0.0-192.168.255.255)\n");
-  cache_enable(pkt);
-  flow_benchmark(pkt, 1);
-  destroy_packet(pkt);
+  for (i = 0; i < n; i++) {
+    pkt[i] = build_ip_packet(1, IPADDR(192,168,1,i), IPADDR(192,168,1,2), 0);
+  }
+  flow_benchmark(pkt, n, 1);
+  printf("\n*** cached 65536 entries %u flow(s) "
+         "(IPV4_SRC 192.168.0.0-192.168.255.255)\n", n);
+  for (i = 0; i < n; i++) {
+    cache_enable(pkt[i]);
+  }
+  flow_benchmark(pkt, n, 1);
+  print_flowcache_stats();
+  for (i = 0; i < n; i++) {
+    destroy_packet(pkt[i]);
+  }
 }
 
 void
-test_IPV4_DST_65536_entries_flow_benchmark(void) {
-  struct lagopus_packet *pkt;
+test_IPV4_SRC_65536_entries_1_flow_benchmark(void) {
+  IPV4_SRC_65536_entries_flow_benchmark(1);
+}
 
-  printf("\n*** 65536 entries (IPV4_DST 192.168.0.0-192.168.255.255)\n");
+void
+test_IPV4_SRC_65536_entries_256_flow_benchmark(void) {
+  IPV4_SRC_65536_entries_flow_benchmark(256);
+}
+
+void
+IPV4_DST_65536_entries_flow_benchmark(size_t n) {
+  struct lagopus_packet *pkt[n];
+  size_t i;
+
+  printf("\n*** 65536 entries %u flow(s) "
+         "(IPV4_DST 192.168.0.0-192.168.255.255)\n", n);
   ipdst_flow_add(IPADDR(192,168,0,0), IPADDR(192,168,255,255), 32);
-  pkt = build_ip_packet(1, IPADDR(192,168,1,1), IPADDR(192,168,1,2), 0);
-  flow_benchmark(pkt, 1);
-  printf("\n*** cached 65536 entries (IPV4_DST 192.168.0.0-192.168.255.255)\n");
-  cache_enable(pkt);
-  flow_benchmark(pkt, 1);
-  destroy_packet(pkt);
+  for (i = 0; i < n; i++) {
+    pkt[i] = build_ip_packet(1, IPADDR(192,168,1,i), IPADDR(192,168,1,2), 0);
+  }
+  flow_benchmark(pkt, n, 1);
+  printf("\n*** cached 65536 entries %u flow(S) "
+         "(IPV4_DST 192.168.0.0-192.168.255.255)\n", n);
+  for (i = 0; i < n; i++) {
+    cache_enable(pkt[i]);
+  }
+  flow_benchmark(pkt, n, 1);
+  print_flowcache_stats();
+  for (i = 0; i < n; i++) {
+    destroy_packet(pkt[i]);
+  }
+}
+
+void
+test_IPV4_DST_65536_entries_1_flow_benchmark(void) {
+  IPV4_DST_65536_entries_flow_benchmark(1);
+}
+
+void
+test_IPV4_DST_65536_entries_256_flow_benchmark(void) {
+  IPV4_DST_65536_entries_flow_benchmark(256);
 }
