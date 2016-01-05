2c52ce9cbdcdd02651d79e1f6f7fd9c320d02b25
hibitomo <hibi.tomoya@lab.ntt.co.jp>
2016-01-06 06:16:33 +0900

    dataplane: Fix dpdk usage

--- src/dataplane/dpdk/config.c
+++ src/dataplane/dpdk/config.c
@@ -62,29 +62,13 @@
 #include <rte_common.h>
 #include <rte_byteorder.h>
 #include <rte_log.h>
-#include <rte_memory.h>
-#include <rte_memcpy.h>
-#include <rte_memzone.h>
-#include <rte_tailq.h>
 #include <rte_eal.h>
 #include <rte_per_lcore.h>
-#include <rte_launch.h>
-#include <rte_atomic.h>
 #include <rte_cycles.h>
-#include <rte_prefetch.h>
 #include <rte_lcore.h>
-#include <rte_branch_prediction.h>
-#include <rte_interrupts.h>
-#include <rte_pci.h>
-#include <rte_random.h>
 #include <rte_debug.h>
 #include <rte_ether.h>
 #include <rte_ethdev.h>
-#include <rte_ring.h>
-#include <rte_mempool.h>
-#include <rte_mbuf.h>
-#include <rte_ip.h>
-#include <rte_tcp.h>
 #include <rte_string_fns.h>
 #include <rte_version.h>
 
@@ -158,6 +142,7 @@ static const char usage[] =
 
 void
 dp_dpdk_thread_usage(FILE *fp) {
+  eal_common_usage(); /* XXX stdout */
   fprintf(fp, usage,
           APP_DEFAULT_NIC_RX_RING_SIZE,
           APP_DEFAULT_RING_RX_SIZE,
